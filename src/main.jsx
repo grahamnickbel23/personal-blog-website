5faf3368d5a51aa338a526634e5b6d8fe996f3aa
Graham <grahamnickbel207official@gmail>
2026-01-19 17:59:47 +0530

    fixed parms issue and added analatycs v1.0.1

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import './index.css'
 import App from './App.jsx'
 
 import { PortfolioProvider } from './context/PortfolioContext.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from '@vercel/analytics/next'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PortfolioProvider>
+      
       <App />
+
+      <Analytics/>
+      <SpeedInsights />
+
     </PortfolioProvider>
   </StrictMode>,
 )
